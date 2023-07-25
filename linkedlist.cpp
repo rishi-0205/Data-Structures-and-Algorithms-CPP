3181507b93a84bcc961c893f00f628f3d6c41e63
rishi-0205 <rishijc02@gmail.com>
2023-07-25 18:56:07 +0530

    Linked List Implemented

---
+++ linkedlist.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+int main(){
+    Node *head = new Node(); 
+    Node *node1 = new Node();
+    Node *node2 = new Node();
+    Node *node3 = new Node();
+    head->data = 10;
+    head->next = node1;
+    node1->data = 11;
+    node1->next = node2;
+    node2->data = 12;
+    node2->next = NULL;
+
+    for(node3 = head; node3 != NULL; node3 = node3->next){
+        std::cout<< node3->data<< '\n';
+    };
+
+
+}
